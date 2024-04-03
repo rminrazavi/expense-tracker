82458224471f22349c0b683415890e6e88e1fb8d
Armin Razavi <arminrazavi79@gmail.com>
2024-04-03 13:50:23 +0330

    project boilerplate setup

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectedDb.js";
+
+dotenv.config();
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
---
+++ backend/typeDefs/transaction.typeDef.js
@@ -0,0 +1,39 @@
+const transactionTypeDef = `#graphql
+type Transaction {
+    _id: ID!
+    userId: ID!
+    description: String!
+    paymentType: String!
+    catagory: String!
+    amount: Float!
+    location: String
+    date: String!
+}
+type Query {
+    transactions: [Transaction!]
+    transaction(transactionId:ID!): Transaction!
+}
+type Mutation {
+    createTransaction(input: createTransactionInput!): Transaction!
+    updateTranaction(input: updateTransactionInput!): Transaction!
+    deleteTransaction(transactionId:ID!): Transaction!
+}
+input createTransactionInput {
+    description: String!
+    paymentType: String!
+    catagory: String!
+    amount: Float!
+    location: String
+}
+input updateTransactionInput {
+    transactionId: ID!
+    description: String
+    paymentType: String
+    catagory: String
+    amount: Float
+    location: String
+    date: String
+}
+`;
+
+export default transactionTypeDef;
